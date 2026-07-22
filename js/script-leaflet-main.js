57df56915e7dc9f9af7629a90a7b1ab8873f9c55
dreelu <andre.luis8173@gmail.com>
2026-07-22 14:19:08 -0300

    feat: alteração nas funcionalidades da página main
    - Os botões voltaram a funcionar e ter funções próprias.
    - Agora três pontos não formam mais automaticamente um triangulo, e o baricentro do mesmo também não é mais automáticamente calculado.

--- js/script-leaflet-main.js
+++ js/script-leaflet-main.js
@@ -1,5 +1,23 @@
+let tool = null;
+
+let botaoPonto = document.querySelector('div#botaoPonto');
+let botaoReta = document.querySelector('div#botaoReta');
+let botaoTriangulo = document.querySelector('div#botaoTriangulo');
+
+botaoPonto.addEventListener('click', () => {
+    tool = "ponto";
+});
+
+botaoReta.addEventListener('click', () => {
+    tool = "reta";
+});
+
+botaoTriangulo.addEventListener('click', () => {
+    tool = "triangulo";
+});
 
 let map = L.map('map').setView([-5.073709, -42.831378], 18);
+
 {     
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 24,
@@ -7,42 +25,67 @@ let map = L.map('map').setView([-5.073709, -42.831378], 18);
     }).addTo(map);
     
     let pontos = [];
+    let pontosRetas = [];
+    let pontosTriangulos = [];
     let marcadores = [];
-    let polygons = [];
+    let marcadoresRetas = [];
+    let marcadoresTriangulos = [];
+    let triangulos = [];
 
     // Pega a coordenada do click, adiciona um marcador e guarda ambos em arrays
     function onMapClick(e) {
-        let marker = L.marker(e.latlng).addTo(map);
-        let point = e.latlng;
-        pontos.push(point);
-        marcadores.push(marker);
-
-
-        /*
-        Se existirem 3 pontos e 3 marcadores:
-            - Criará um polygono com os 3 pontos
-            - Remove todos os marcadores e pontos salvos em seus respectivos arrays
-            - Pegará as coordenadas do baricentro e adiciona um marcador
-            - Adiciona o marcador e a coordenada do baricentro nos arrays
-        */
-        
-        if (pontos.length == 3 && marcadores.length == 3) {
-            polygons.forEach(polygon => map.removeLayer(polygon));
-            let polygon = L.polygon([pontos[0], pontos[1], pontos[2]], { color: 'red', fill: true }).addTo(map);
-            polygons.push(polygon)
-
-            marcadores.forEach(marker => 
-                map.removeLayer(marker)
-            );
-            marcadores.length = 0;
-            pontos.length = 0;
-
-            let marker = L.marker(polygon.getCenter()).addTo(map);
-            let point = polygon.getCenter();
-
+        if (tool === "ponto") {
+            let point = e.latlng;
+            let marker = L.marker(point).addTo(map);
             pontos.push(point);
             marcadores.push(marker);
+
+            tool = null;
         }
+
+        if (tool === "reta") {
+            let point = e.latlng;
+            let marker = L.marker(point).addTo(map);
+            pontosRetas.push(point);
+            marcadoresRetas.push(marker);
+
+            if (pontosRetas.length >= 2 && marcadoresRetas.length >= 2) {
+                let line = L.polyline(pontosRetas).addTo(map);
+                
+                for (let i = 0; i < marcadoresRetas.length; i++) {
+                    map.removeLayer(marcadoresRetas[i]);
+                }
+
+                pontosRetas.length = 0;
+                marcadoresRetas.length = 0;
+
+                tool = null;
+            }
+        }
+
+        if (tool === "triangulo") {
+
+
+            let point = e.latlng;
+            let marker = L.marker(point).addTo(map);
+            pontosTriangulos.push(point);
+            marcadoresTriangulos.push(marker);
+
+            if (pontosTriangulos.length >= 3 && marcadoresTriangulos.length >= 3) {
+                let polygon = L.polygon(pontosTriangulos).addTo(map);
+
+                for (let i = 0; i < marcadoresTriangulos.length; i++) {
+                    map.removeLayer(marcadoresTriangulos[i]);
+                }
+
+                pontosTriangulos.length = 0;
+                marcadoresTriangulos.length = 0;
+                triangulos.push(polygon);
+                
+                tool = null;
+            }
+        }
+
     }
     map.on('click', onMapClick);
 }
